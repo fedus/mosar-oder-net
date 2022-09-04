47bc25b81890991ffdb248480bd9d5dca9447b41
Federico Gentile <gentfede+git@gmail.com>
2022-09-04 23:28:24 +0200

    feat: wippedy wip

--- src/routes/play/finish/+page.svelte
+++ src/routes/play/finish/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
     import Mosavatar from '$lib/components/mosavatar.svelte';
     import aperol from "$lib/assets/mosavatars/aperol.png";
 
@@ -6,7 +7,11 @@
 
     const { score, rounds } = gameStateStore;
 
+    let ready = false;
+
     $: mosavatar_index = $score == 0 ? 0 : $score - 1;
+
+    onMount(() => ready = true);
 </script>
 
 <div class="score-container">
@@ -17,10 +22,13 @@
     <div class="mosavatar-score-container">
         <div class="score-aperol">
             {#each [...Array(5).keys()] as idx}
-                <img src={aperol} class={`score-${idx + 1}`} class:inactive={idx + 1 > $score} alt="Aperol Spritz">
+                {@const className = `score-${idx + 1}`}
+                <img src={aperol} class={`aperol ${ready ? className : ''}`} class:inactive={idx + 1 > $score} alt="Aperol Spritz">
             {/each}
         </div>
-        <Mosavatar mainSrc={`/mosavatars/m_score_${mosavatar_index}.png`} />
+        <div class="mosavatar">
+            <Mosavatar mainSrc={`/mosavatars/m_score_${mosavatar_index}.png`} />
+        </div>
     </div>
 </div>
 
@@ -48,29 +56,47 @@
             bottom: 0;
 
             .score-aperol {
-                img {
+                .aperol {
+                    position: absolute;
+                    left: 50%;
+                    transform: translateX(-50%) translateY(50px);
                     width: 100px;
+
+                    transition: transform 1s;
                 }
 
                 .score-1 {
-                    transform: translate(-20px, 80px) rotate(-40deg);
+                    transform: translateX(-250%) translateY(-100px) rotate(-40deg);
                 }
 
                 .score-2 {
-                    transform: translate(-20px, 20px) rotate(-20deg);
+                    transform: translateX(-150%) translateY(-150px) rotate(-20deg);
+                    transition-delay: .3s;
+                }
+
+                .score-3 {
+                    transform: translateX(-50%) translateY(-170px);
+                    transition-delay: .6s;
                 }
 
                 .score-4 {
-                    transform: translate(20px, 15px) rotate(20deg);
+                    transform: translateX(50%) translateY(-155px) rotate(20deg);
+                    transition-delay: .9s;
                 }
 
                 .score-5 {
-                    transform: translate(20px, 70px) rotate(40deg);
+                    transform: translateX(140%) translateY(-110px) rotate(40deg);
+                    transition-delay: 1.2s;
                 }
 
                 .inactive {
                     filter: grayscale(100%);
                 }
+            }
+
+            .mosavatar {
+                position: relative;
+                z-index: 1;
             }
         }
     }
